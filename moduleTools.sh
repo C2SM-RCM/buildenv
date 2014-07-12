a63f5ed53f8b68e6ebffe462eca8569bdcaf2055
Oliver Fuhrer <oliver.fuhrer@meteoswiss.ch>
2014-07-12 12:53:29 +0000

    - update error and warning functions

--- moduleTools.sh
+++ moduleTools.sh
@@ -9,16 +9,17 @@
 exitError()
 {
     \rm -f /tmp/tmp.$$ 1>/dev/null 2>/dev/null
-    echo "ERROR $1: LINE=$2" 1>&2
-    PARENT_COMMAND=$(ps $PPID | tail -n 1 | awk "{print \$6}")
-    echo "ERROR       LOCATION=$0"
+    echo "ERROR $1: $3" 1>&2
+    echo "ERROR     LOCATION=$0" 1>&2
+    echo "ERROR     LINE=$2" 1>&2
     exit $1
 }
 
 showWarning()
 {
-    echo "WARNING $1: LINE=$2" 1>&2
-    echo "WARNING       LOCATION=$0"
+    echo "WARNING $1: $3" 1>&2
+    echo "WARNING       LOCATION=$0" 1>&2
+    echo "WARNING       LINE=$2" 1>&2
 }
 
 containsElement()

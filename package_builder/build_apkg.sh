e1b2795781f3637319cb7718f03e283cf0f2cfbd
Pascal Spörri <pascal.spoerri@env.ethz.ch>
2016-01-29 14:16:02 +0100

    Adapting apckg

--- package_builder/build_apkg.sh
+++ package_builder/build_apkg.sh
@@ -50,10 +50,10 @@ source ${envloc}/moduleTools.sh
 
 fwd_args="${fwd_args} -d ${package_basedir} -i ${installdir}"
 
-
-if [[ $package == "sertools" ]]; then
-    echo "Building specific package: ${BASEPATH_SCRIPT}/build_sertools.sh $fwd_args"
-    ${BASEPATH_SCRIPT}/build_sertools.sh $fwd_args
+package_buildscript="${envloc}/build_${package}.sh"
+if [ ! -f "${package_buildscript}" ] ; then
+    echo "Building specific package: ${BASEPATH_SCRIPT}/build_${package}.sh $fwd_args"
+    ${package_buildscript} $fwd_args
 else 
     exitError 2221 ${LINENO} "Package ${package} not known"
 fi

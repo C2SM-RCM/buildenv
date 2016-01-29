bed0502e1c998ec890de206ec6a7517ae8772ed3
Pascal Spörri <pascal.spoerri@env.ethz.ch>
2016-01-29 14:25:56 +0100

    Fixed build_apkg to use package based paths

--- package_builder/build_apkg.sh
+++ package_builder/build_apkg.sh
@@ -51,9 +51,10 @@ source ${envloc}/moduleTools.sh
 fwd_args="${fwd_args} -d ${package_basedir} -i ${installdir}"
 
 package_buildscript="${BASEPATH_SCRIPT}/build_${package}.sh"
-if [ ! -f "${package_buildscript}" ] ; then
-    echo "Building specific package: ${BASEPATH_SCRIPT}/build_${package}.sh $fwd_args"
+if [ -f $package_buildscript ] ; then
+    echo "Building specific package: ${package_buildscript} $fwd_args"
     ${package_buildscript} $fwd_args
-else 
+else
+    echo ${package_buildscript}
     exitError 2221 ${LINENO} "Package ${package} not known"
 fi

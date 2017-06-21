507d52de9ba0185c00d6639b6de876322c27e3f3
Katherine Osterried <katherine.osterried@env.ethz.ch>
2017-06-21 13:24:33 +0200

    Add script for Jenkins to test the testsuite

---
+++ package_test/test_testsuite.sh
@@ -0,0 +1,14 @@
+#!/bin/bash -e
+
+if [ "${host}" == "kesch" ] || [ "${host}" == "kesch-pgi" ]; then
+  module load git/2.8.4
+fi
+
+cd ..
+git clone git@github.com:MeteoSwiss-APN/cosmo-pompa
+cd cosmo-pompa
+git remote add -f testsuite git@github.com:C2SM-RCM/testsuite
+git subtree pull --prefix cosmo/test/testsuite/src/ testsuite ${BRANCH} --squash -m "Update testsuite"
+cd cosmo/test
+test -f ./jenkins/jenkins.sh || exit 1
+./jenkins/jenkins.sh test

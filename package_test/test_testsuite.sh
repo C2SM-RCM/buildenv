4bae5b0ad74d0748a5c972f9e288ed5235e02f6e
Katherine Osterried <katherine.osterried@env.ethz.ch>
2017-06-22 15:14:16 +0200

    Add int2lm to Jenkins testsuite plan

--- package_test/test_testsuite.sh
+++ package_test/test_testsuite.sh
@@ -1,5 +1,7 @@
 #!/bin/bash -e
 module load git/2.8.4
+
+# First, test cosmo-pompa
 git clone git@github.com:MeteoSwiss-APN/cosmo-pompa
 cd cosmo-pompa
 git config user.email "jenkins@cscs.ch"
@@ -9,3 +11,16 @@ git subtree pull --prefix cosmo/test/testsuite/src/ testsuite ${BRANCH} --squash
 cd cosmo/test
 test -f ./jenkins/jenkins.sh || exit 1
 ./jenkins/jenkins.sh test
+
+# Next, test int2lm
+cd ../../..
+git clone git@github.com:MeteoSwiss-APN/int2lm
+cd int2lm
+git config user.email "jenkins@cscs.ch"
+git config user.name "Mr. Jenkins"
+git remote add -f testsuite git@github.com:C2SM-RCM/testsuite
+git subtree pull --prefix test/testsuite/src/ testsuite ${BRANCH} --squash -m "Update testsuite"
+cd test
+test -f ./jenkins/jenkins.sh || exit 1
+./jenkins/jenkins.sh test
+

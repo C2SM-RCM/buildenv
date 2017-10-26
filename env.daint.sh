2186f456501b20eb7f85e1ef0fe65271f5c6408b
Pascal Spörri <pspoerri@users.noreply.github.com>
2017-10-26 09:47:20 +0200

    Update boost path on daint

--- env.daint.sh
+++ env.daint.sh
@@ -29,7 +29,7 @@ setupDefaults()
     fcompiler_cmds=(ftn)
 
     # Module display boost
-    export BOOST_PATH="/project/c14/install/daint/boost"
+    export BOOST_PATH="/project/c14/install/daint/boost/boost_1_49_0"
 
     # Check if ncurses was loaded before
     export BUILDENV_NCURSES_LOADED=`module list -t 2>&1 | grep "ncurses"`

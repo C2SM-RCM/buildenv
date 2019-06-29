9a047b2419dc091719968f02b109a0700d01090d
cosunae <carlos.osuna@meteoswiss.ch>
2019-06-29 13:04:38 +0200

    add boost configuration for arolla

--- env.arolla.sh
+++ env.arolla.sh
@@ -53,7 +53,12 @@ setupDefaults()
     export NVIDIA_CUDA_ARCH="sm_70"
 
     # BOOST
-    export BOOST_PATH="/users/jenkins/Code/boost-1.49.0/"
+    export Boost_NO_SYSTEM_PATHS=true
+    export Boost_NO_BOOST_CMAKE=true
+
+    export BOOST_ROOT=/project/c14/install/kesch/boost/boost_1_67_0/
+    export BOOST_PATH=${BOOST_ROOT}
+    export BOOST_INCLUDE=${BOOST_ROOT}/include/
 
     # default options
     if [ -z "${target}" ] ; then

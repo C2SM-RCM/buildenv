5bc69c67637891cffce85b1d59fb16cabf7a3742
Guy de Morsier <guy.demorsier@meteoswiss.ch>
2019-12-17 12:09:11 +0100

    Launch testsuite regression test (#88)
    added make test

--- package_test/test_testsuite.sh
+++ package_test/test_testsuite.sh
@@ -20,6 +20,8 @@ cd testsuite
 git checkout ${BRANCH}
 echo "Last commit in testsuite repo:"
 git --no-pager log -1
+# Launch testsuite regression tests
+make test
 cd $wd
 
 # First, test cosmo-pompa

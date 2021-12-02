2ef6670bf02a7cda749952d1b568745600d65998
Matthieu Leclair <matthieu.leclair@env.ethz.ch>
2021-12-02 12:14:13 +0100

    FIX: replace c14 dependencies and switch off unit testing

--- env.daint.sh
+++ env.daint.sh
@@ -30,9 +30,15 @@ setupDefaults()
 
     # Module display boost
     # export BOOST_PATH="/project/c14/install/daint/boost/boost_1_49_0"
-    export BOOST_PATH="/project/g110/spack-install/daint/boost/1.67.0/gcc/hqbmwq4tbkcfhwo5kjrhkpns3vvavil2"
-    export serialbox_root="/project/pr94/spack-install/serialbox/v2.0.1/gcc/yr5lflmpxmgabwfk5ugsnlfh6teswv3g"
-
+    export BOOST_PATH="/project/pr94/cosmo-pompa_dependencies/boost_1_49_0"
+    export ENABLE_SERIALIZATION="OFF"
+    export ENABLE_BENCHMARK="OFF"
+    export ENABLE_TESTING="OFF"
+    # export serialbox_root="/project/g110/spack-install/daint/serialbox/2.6.0/gcc/lpqcyjrpedew76gskfy4jyetb6klapgo"
+    # export serialbox_root="/project/pr94/spack-install/serialbox/v2.0.1/gcc/yr5lflmpxmgabwfk5ugsnlfh6teswv3g"
+    # export serialbox_root="project/pr94/spack-install/serialbox/2.4.3/gcc/y2mspebcxsl7abvyrgqfqzepqjw77ltc"
+    # export serialbox_root="/project/pr94/leclairm/spack-install/serialbox/2.4.3/gcc/y2mspebcxsl7abvyrgqfqzepqjw77ltc"
+    
     # Check if ncurses was loaded before
     export BUILDENV_NCURSES_LOADED=`module list -t 2>&1 | grep "ncurses"`
 
